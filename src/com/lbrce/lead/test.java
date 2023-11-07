1306bcb97eabc95d366bafa9412a563b8baf289e
DELL <DELL@DESKTOP-393TETE>
2023-11-08 04:05:27 +0530

    first commit

---
+++ src/com/lbrce/lead/test.java
@@ -0,0 +1,5 @@
+package com.lbrce.lead;
+
+public class test {
+
+}
---
+++ src/gitprjt1/test.java
@@ -0,0 +1,12 @@
+package gitprjt1;
+
+public class test {
+  public static void main(String args[]) {
+	  int x=0;
+	if(x!=0)
+		System.out.println("yes");
+	else
+		System.out.println("No");
+
+}
+}
